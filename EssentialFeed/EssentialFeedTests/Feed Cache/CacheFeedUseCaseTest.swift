04886d729cb54b4adbcf9fd4baa43942e0e1121e
Bukhari Muslim <bm.bukharimuslim@gmail.com>
2025-05-12 09:22:35 +0700

    Save command request cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTest.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTest.swift
@@ -9,13 +9,23 @@ import XCTest
 @testable import EssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed()
     }
 }
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+    
+    func deleteCacheFeed() {
+        deleteCacheFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTest: XCTestCase {
@@ -25,5 +35,30 @@ class CacheFeedUseCaseTest: XCTestCase {
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+    
+    func test_save_reqeuestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    // MARK : - Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(
+            id: UUID(),
+            description: "any",
+            location: "any",
+            imageURL: anyURL()
+        )
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
 }
