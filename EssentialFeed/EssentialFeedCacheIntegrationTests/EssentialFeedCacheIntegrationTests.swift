34aaf0d11bbbeb2f2eb7158c9ff400283363db1c
Bukhari Muslim <bm.bukharimuslim@gmail.com>
2025-05-16 18:28:29 +0700

    Add `EssentialFeedCacheIntegrationTest` target to separate the potentially-slower cache integration test from the fast unit/isolated test

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Bukhari Muslim on 16/05/25.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
