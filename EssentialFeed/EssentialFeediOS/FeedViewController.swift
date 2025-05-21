21ca030d16ca8cb18dd5069f5d266c30a97de44a
Bukhari Muslim <bm.bukharimuslim@gmail.com>
2025-05-22 00:21:04 +0700

    Call startTask related to iOS 15 Update since we only call `willDisplayCell`

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -99,15 +99,23 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
-            let cellModel = tableModel[indexPath.row]
-            tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { _ in }
+            startTask(forRowAt: indexPath)
         }
+    }
+    
+    public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        startTask(forRowAt: indexPath)
     }
     
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
             cancelTask(forRowAt: indexPath)
         }
+    }
+    
+    private func startTask(forRowAt indexPath: IndexPath) {
+        let cellModel = tableModel[indexPath.row]
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { _ in }
     }
     
     private func cancelTask(forRowAt indexPath: IndexPath) {
