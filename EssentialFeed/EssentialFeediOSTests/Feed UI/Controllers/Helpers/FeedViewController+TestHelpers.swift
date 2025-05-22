5c2293710d93d3686eae967d2c4d2063701f7e35
Bukhari Muslim <bm.bukharimuslim@gmail.com>
2025-05-22 10:11:07 +0700

    Remove mutable state from `FeedViewModel`. The `FeedViewModel` only needs to forward state changes, so state is only transient.

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedViewController+TestHelpers.swift
@@ -58,7 +58,7 @@ extension FeedViewController {
         return 0
     }
     
-    func simulateAppearing() {
+    func simulateAppearance() {
         if !isViewLoaded  {
             loadViewIfNeeded()
             replaceRefreshControlWithFakeForiOS17Support()
