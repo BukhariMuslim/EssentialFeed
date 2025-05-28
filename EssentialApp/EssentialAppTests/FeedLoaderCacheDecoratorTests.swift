426942e9edcc7e212552d086167885a6c9492bf6
Bukhari Muslim <bm.bukharimuslim@gmail.com>
2025-05-28 21:40:06 +0700

    `FeedLoaderCacheDecorator.load` does not cache feed on loader failure

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -25,7 +25,9 @@ final class FeedLoaderCacheDecorator: FeedLoader {
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
         decoratee.load { [weak self] result in
-            self?.cache.save((try? result.get()) ?? []) { _ in }
+            if let feed = try? result.get() {
+                self?.cache.save(feed) { _ in }                
+            }
             completion(result)
         }
     }
@@ -49,12 +51,15 @@ class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     
     func test_load_cachesLoadedFeedOnLoaderSuccess() {
         let cache = CacheSpy()
-        let feed = uniqueFeed()
-        let sut = makeSUT(loaderResult: .success(feed), cache: cache)
+        let sut = makeSUT(loaderResult: .failure(anyNSError()), cache: cache)
         
         sut.load { _ in }
         
-        XCTAssertEqual(cache.messages, [.save(feed)], "Expected to cache loaded feed on success")
+        XCTAssertTrue(cache.messages.isEmpty, "Expected not to cache feed on load error")
+    }
+    
+    func test_load_doesNotCacheOnLoaderFailure() {
+        
     }
     
     // MARK: - Helpers
