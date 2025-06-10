52cfb6d5dfb1e8c6784eb6e426534b8aa7d5e049
Bukhari Muslim <bm.bukharimuslim@gmail.com>
2025-06-10 10:46:50 +0700

    Remove duplication

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -34,7 +34,7 @@ extension CoreDataFeedStore: FeedStore {
     public func deleteCacheFeed(completion: @escaping DeletionCompletion) {
         perform { context in
             completion(Result {
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+                try ManagedCache.daleteCache(in: context)
             })
         }
     }
