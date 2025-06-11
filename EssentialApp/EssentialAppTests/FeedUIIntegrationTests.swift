1904400a033979b255d5fe955db227e959ccc27d
Bukhari Muslim <bm.bukharimuslim@gmail.com>
2025-06-11 11:07:01 +0700

    Use UIDiffableDataSource.apply to perform diff and only update what's changed (with animation) Since the diffable data source diffing happens on its own dedicated queue (not the main queue) that may also run work on the main thread, we had to replace the `ImmediateWhenOnMainQueueScheduler` with the `ImmediateWhenOnMainThreadScheduler`.
    Also, we had to rearrange the setup of some tests since the diffable data source eagerly preloads cells.

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -185,7 +185,7 @@ class FeedUIIntegrationTests: XCTestCase {
     func test_loadMoreActions_requestMoreFromLoader() throws {
         let (sut, loader) = try makeSUT()
         sut.simulateAppearance()
-        loader.completeFeedLoading()
+        loader.completeFeedLoading(with: [makeImage()])
         
         XCTAssertEqual(loader.loadMoreCallCount, 0, "Expected no requests before until load more action")
         
@@ -214,13 +214,13 @@ class FeedUIIntegrationTests: XCTestCase {
         sut.simulateAppearance()
         XCTAssertFalse(sut.isShowingLoadMoreFeedIndicator, "Expected no loading indicator once view appears")
         
-        loader.completeFeedLoading(at: 0)
+        loader.completeFeedLoading(with: [makeImage()], at: 0)
         XCTAssertFalse(sut.isShowingLoadMoreFeedIndicator, "Expected no loading indicator once loading completes successfully")
         
         sut.simulateLoadMoreFeedAction()
         XCTAssertTrue(sut.isShowingLoadMoreFeedIndicator, "Expected loading indicator on load more action")
         
-        loader.completeLoadMore(at: 0)
+        loader.completeLoadMore(with: [makeImage()],at: 0)
         XCTAssertFalse(sut.isShowingLoadMoreFeedIndicator, "Expected no loading indicator once user initiated loading completes successfully")
         
         sut.simulateLoadMoreFeedAction()
@@ -433,9 +433,9 @@ class FeedUIIntegrationTests: XCTestCase {
         let (sut, loader) = try makeSUT()
         
         sut.simulateAppearance()
-        loader.completeFeedLoading(with: [image0, image1])
         XCTAssertEqual(loader.loadedImageURLs, [], "Expected no image URL requests until image is near visible")
         
+        loader.completeFeedLoading(with: [image0, image1])
         sut.simulateFeedImageViewNearVisible(at: 0)
         XCTAssertEqual(loader.loadedImageURLs, [image0.url], "Expected first image URL request once first image is near visible")
         
